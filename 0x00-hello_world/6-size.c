99971739cdf3259a061a6e1a34dfb90ce492bb23
(nombre de usuario) <(correo de github)>
2020-02-06 04:05:11 +0000

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ int a;
+long int b;
+long long int c;
+float f;
+char d;
+
+printf ("Size of a char:%lu bytes(s)\n";(unsigned long) size of (d));
+printf ("Size of a int:%lu bytes(s)\n";(unsigned long) size of (a));
+printf ("Size of a long int:%lu bytes(s)\n";(unsigned long) size of (b));
+printf ("Size of a long long int:%lu bytes(s)\n";(unsigned long) size of (c));
+printf ("Size of a float:%lu bytes(s)\n";(unsigned long) size of (f));
+    return (0);
+}
