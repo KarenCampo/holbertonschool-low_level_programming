82d37d960137e50c72166204350ede852096aee8
(nombre de usuario) <(correo de github)>
2020-02-07 04:02:13 +0000

    lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c
+	char C
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar (c);
+	}
+	for (C = 'A' ; C <= 'Z' ; C++)
+	{
+		putchar (C);
+	}
+	putchar('\n');
+
+	return (0);
