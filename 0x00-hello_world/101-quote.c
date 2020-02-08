9cfb77bfa41f7abc7bead259a09ac5290e688271
(nombre de usuario) <(correo de github)>
2020-02-08 00:33:36 +0000

    script quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,9 @@ int main ()
 {
 	int 1;
 	
-	1 = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	1 = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+	write(2,
+	"and that piece of art is useful - Dora Korpar, 2015- 10-19\n", 1);
 	return (1);
 	
 
