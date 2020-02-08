044a7d443602edfeec2bca5a5123031ff5b7156f
(nombre de usuario) <(correo de github)>
2020-02-08 00:55:42 +0000

    fixed Dora's quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-int main ()
+#include <unistd.h>
+/**
+*main - prints line
+* Return: 1
+*/
+int main(void)
 {
-	int 1;
 	
-	1 = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
 	write(2,
-	"and that piece of art is useful - Dora Korpar, 2015- 10-19\n", 1);
+	"and that piece of art is useful\" - Dora Korpar, 2015- 10-19\n", 59);
 	return (1);
 	
 
