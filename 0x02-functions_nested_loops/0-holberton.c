3f0892ca168c19197602fe4bc41a49e6538cc008
Karen Campo <novastilo@yahoo.com>
2020-02-11 21:11:00 +0000

    defined main

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,25 @@
 #include "holberton.h"
 
 /**
- * _putchar - writes the character to stdout
+ *
+ * Main - defines the main function of the program 
+ * putchar - writes the character to stdout
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and error is set appropriately.
  */
 int main(void)
-{ 
-	char name[] = "Holberton";
+{
+	char name[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+
 	int k = 0;
+
 	while (name[k] != '\0')
-{ 
-	_putchar(name[k]); 
+	{
+	_putchar(name[k]);
 	k++;
-}
+	}
 	_putchar('\n');
 	return (0);
 }
