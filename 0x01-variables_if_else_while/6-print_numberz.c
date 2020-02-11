10090fe57dc0ae3bdc1c3235a7751057fc986242
(nombre de usuario) <(correo de github)>
2020-02-11 03:43:39 +0000

    compiled numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - stars, entry
+ *
+ * Return: 0 
+ */
+
+int main(void)
+{
+	int num = 0;
+	while (num < 10)
+{ 
+	putchar(num + '0');
+	num++;
+}
+	putchar('\n');
+	return (0);
+}
