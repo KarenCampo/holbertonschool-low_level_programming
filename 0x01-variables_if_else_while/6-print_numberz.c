7f8c4f0c5ac0363ab0a1308e52c6cb60ce9806a6
(nombre de usuario) <(correo de github)>
2020-02-11 04:09:57 +0000

    fixed trailed space

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - starts, entry
  *
- * Return: 0 (Succes)
+ * Return: 0 
  */
 
 int main(void)
 {
 	int num = 0;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0 ; num <= 9 ; num++)
 { 
 	putchar(num + 48);
 }
