d49522a4159a6ab41f97451d8f170b377788a387
Karen Campo <novastilo@yahoo.com>
2020-02-11 21:37:42 +0000

    fixed error

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,8 +6,7 @@
  * putchar - writes the character to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
+ * Return: On success 0.
  */
 int main(void)
 {
