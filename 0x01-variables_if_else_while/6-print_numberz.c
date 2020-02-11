8e7207027e7d9811a4a29a6c6c2b167d82a3576b
(nombre de usuario) <(correo de github)>
2020-02-11 04:05:50 +0000

    please work!

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,15 +5,16 @@
 /**
  * main - starts, entry
  *
- * Return: 0 
+ * Return: 0 (Succes)
  */
 
 int main(void)
 {
 	int num = 0;
+
 	for (num = 0; num <= 9; num++)
 { 
-	putchar (num + 48);
+	putchar(num + 48);
 }
 	putchar('\n');
 	return (0);
