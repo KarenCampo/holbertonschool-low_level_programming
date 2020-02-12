f236169cb0533d4e2f6fc119ba8a670f9c779a2b
Karen Campo <novastilo@yahoo.com>
2020-02-12 02:55:36 +0000

    test

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ */
+int print_last_digit(int n)
+{
+int last;
+
+last = n;
+
+if (last >= 10)
+last  = last % 10;
+else if (last < 0 && last <= -10)
+last = last % 10;
+
+if (last < 0)
+last = last * -1;
+
+_putchar(last + '0');
+return (last);
+}
