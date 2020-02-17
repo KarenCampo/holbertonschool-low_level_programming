6a60b81800de677435bc152c41861cb4be341710
Karen Campo <novastilo@yahoo.com>
2020-02-17 19:32:47 +0000

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry Point.
+ * Return: 0
+ */
 
 int main(void)
 {
 	int number = 48;
+
 	for ( ; number <= 57; number++)
 		putchar(number);
 	putchar(10);
-	return(0);
+	return (0);
 }
