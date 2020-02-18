bdcb1b8cd05ae9cc12266bba7317e1e4b11ba756
Karen Campo <novastilo@yahoo.com>
2020-02-18 21:44:33 +0000

    update

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,7 @@
+#define HOLBERTON_H
+#ifndef HOLBERTON_H
+
+#include <stdio.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -8,3 +12,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
