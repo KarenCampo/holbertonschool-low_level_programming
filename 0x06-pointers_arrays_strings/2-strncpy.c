bba6e8ecf997a29a7a0f4c834c92ea696aa8e063
Karen Campo <novastilo@yahoo.com>
2020-02-22 01:08:23 +0000

    function works like strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
- /**
- * _strncpy - concatenates strings.
- * @dest: concatenates
- * @src: concatenated
- * @n: quantity
- * Return: dest
+/**
+* _strncpy - concatenates strings.
+* @dest: concatenates
+* @src: concatenated
+* @n: quantity
+* Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
+
 {
 	int i;
 
