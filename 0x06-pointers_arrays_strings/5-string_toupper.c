eb3be19a36369cb440669aa705ddf314bad82f48
Karen Campo <novastilo@yahoo.com>
2020-02-24 16:16:34 +0000

    upload

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * *string_toupper - string function
+ * @p: character to be changed
+ * Return: p = string_toupper(s)
+ */
+char *string_toupper(char *p)
+{
+	int i = 0;
+
+	for ( ; p[i] != '\0'; i++)
+		if (p[i] >= 97 && p[i] <= 122)
+		{
+			p[i] -= 32;
+		}
+	return (p);
+}
