ca447cd5204f13b14ed7ea03ef1b971beecec0e5
Karen Campo <novastilo@yahoo.com>
2020-02-26 02:58:39 +0000

    add task 1

---
+++ 0x07-pointers_array/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _memset - function that fills the fist n
+ * @n: number of bytes of the momery area pointed to
+ * @s: memory area
+ * @b: constant byte
+ * Return: n
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for ( ; n; i++)
+	{
+		*(s + i) = b;
+		n--;
+	}
+return (s);
+}
