f686e26acb77738f41447b5dfd7ab02f301f12c3
Karen Campo <novastilo@yahoo.com>
2020-02-26 21:38:28 +0000

    upload task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - Entry point
+ * @s: string to print
+ * Return: 0 on success
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar(10);
+}
