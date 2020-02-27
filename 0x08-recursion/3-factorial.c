15f300b590ccc0e8644f8a1701be93feee130852
Karen Campo <novastilo@yahoo.com>
2020-02-27 01:01:59 +0000

    add task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+B#include "holberton.h"
+/**
+ * factorial - Entry point
+ * @n: given number
+ * Return: 1 on success, -1 when error
+ */
+int factorial(int n)
+
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n > 0)
+	{
+		return (n * (factorial(n - 1)));
+	}
+			return (0);
+}
