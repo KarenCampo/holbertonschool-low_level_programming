4e4e77167e6901846e4e047385057ae8a6b26bbc
Karen Campo <novastilo@yahoo.com>
2020-02-28 00:39:06 +0000

    add task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,11 +9,12 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int j = 0;
+	int j;
 	unsigned int count = 0;
 
 	for ( ; *s != '\0'; s++)
 	{
+		j = 0;
 		while (accept[j] != '\0')
 		{
 			if (*s == accept[j])
