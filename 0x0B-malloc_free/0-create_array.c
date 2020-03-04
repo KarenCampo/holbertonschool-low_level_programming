c674ecccfd906ef30c3f9fcf4d21b4226811b7a1
(nombre de usuario) <(correo de github)>
2020-03-04 04:40:31 +0000

    add task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+  * create_array - for a char array 
+  * @size: size variable
+  * @c: counter variable
+  * Return: char
+  */
+char *create_array(unsigned int size, char c)
+{
+char *l;
+
+	if (!size)
+		return (NULL);
+
+	l = malloc(sizeof(char) * size);
+
+	if (!l)
+		return (NULL);
+	if (l)
+	{
+		while (size > 0)
+			l[--size] = c;
+	}
+	return (l);
+}
