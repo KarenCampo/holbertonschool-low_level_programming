b3b4eb01b4442c90d6173e033d1dedba3ec18193
Karen Campo <novastilo@yahoo.com>
2020-03-09 19:46:02 +0000

    add task 4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef _4_SUM_H_
+#define _4_SUM_H_
+/**
+ * 4-sum - function that computes the sum of X and y
+ * @y: number to compute
+ * @x: number to compute
+ */
+
+#define SUM(x, y) ((x + y))
+
+#endif
