951b7dfc82ccb56ea4aa7723d4421cc9719d5927
Karen Campo <novastilo@yahoo.com>
2020-03-09 23:57:28 +0000

    add the fist task of this project

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct_dog - information about the dog
- * @d: pointer
+ * struct dog - information about the dog
  * @name: name of the dog type char
  * @age: age of the dog type float
  * @owner: owner of the dog type char
  *
  */
 struct dog
-
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 #endif
