1939b41e80ace079504b34a16983db49c947a4c1
Karen Campo <novastilo@yahoo.com>
2020-03-09 18:58:10 +0000

    add task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+/**
+ * 3_FUNCTION_LIKE_MACRO - function that computes the absolute value of x
+ * @x: number to be compute
+ */
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x) * -1)
+
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
