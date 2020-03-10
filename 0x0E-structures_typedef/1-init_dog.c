06db2183a76a1ce237433b16ce952c3a9360e9f2
Karen Campo <novastilo@yahoo.com>
2020-03-10 00:10:43 +0000

    add task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,14 @@
 #include "dog.h"
-
 /**
  * init_dog - Entry Point
- * @d: pointer to structure
+ * @d: pointer to the dog structure
+ * @name: name of the dog type char
+ * @age: age of the dog float type
+ * @owner: information of the dog owner char type
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
+
 {
 	if (d)
 	{
