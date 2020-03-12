95699fc50396e9dc57e18a786d54ad03474ee0c7
(nombre de usuario) <(correo de github)>
2020-03-12 02:58:09 +0000

    add header file for this project

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTER_H_
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+
+#endif
