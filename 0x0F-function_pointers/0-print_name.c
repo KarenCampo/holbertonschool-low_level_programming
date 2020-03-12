717f66c7dd8f0d3c5a9f779597489ad1a1917c42
(nombre de usuario) <(correo de github)>
2020-03-12 02:19:37 +0000

    add task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - this function prints a name.
+ * @name: pinter char type
+ * @f: function pointer char type
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
