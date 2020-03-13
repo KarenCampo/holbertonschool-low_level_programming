18357a2309da935f8c711de4b206f448e8a1f99b
Karen Campo <novastilo@yahoo.com>
2020-03-13 17:30:55 +0000

    add header file for this project

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_fUNCTION_H
+#define VARIADIC_FUNCTION_H
+/**
+ * variadic_function - accepts a variable number of arguments
+ */
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
