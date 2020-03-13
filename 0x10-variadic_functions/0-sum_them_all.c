c74c736b575f78efa60aa702953c13c67c9e10a5
Karen Campo <novastilo@yahoo.com>
2020-03-13 20:56:25 +0000

    add task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all
+ * @n: number of elements
+ * Return - if n = 0, return 0
+ */
+int sum_them_all(const unsigned int n, ...)
+
+{
+	va_list a;
+	unsigned int i = 0;
+	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(a, n); /*initialize a for n number*/
+
+
+	for ( ; i < n; i++) /*access all the arguments assigned to a*/
+	{
+		sum += va_arg(a, int);
+	}
+	va_end(a); /*cleans memory reserved for a*/
+	return (sum);
+}
