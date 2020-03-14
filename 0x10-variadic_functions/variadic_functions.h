de600154a782319af3403610d6d6e550d53cde7b
Karen Campo <novastilo@yahoo.com>
2020-03-14 02:28:41 +0000

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct t_arg
+{
+	char *t_arg;
+	void (*f)(va_list);
+}t_argjr;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
