426034b2a6f0a19f54f2e0bf04ac025c8db590e4
(nombre de usuario) <(correo de github)>
2020-03-20 04:19:04 +0000

    add task 0 for this projet

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,8 +2,8 @@
 
 /**
  * print_list - This funtion prints all the elements of a list_t
- * @h: head of the linked list
- * Return: number of nodes
+  * @h: head of the linked list
+ * Return: returns the number of nodes
  */
 size_t print_list(const list_t *h)
 {
