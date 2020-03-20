b3e3eb7596994f8f407e8ce027429e1c5f09dacd
(nombre de usuario) <(correo de github)>
2020-03-20 04:14:49 +0000

    add task 0 for this projet

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 
 /**
  * print_list - This funtion prints all the elements of a list_t
- * @h: head of the linked list 
+ * @h: head of the linked list
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
@@ -12,15 +12,12 @@ size_t print_list(const list_t *h)
 	for ( ; h; counter++)
 	{
 		if (h->str == NULL)
-		{ 
+		{
 			printf("[0] (nil)\n");
 		}
 		else
-		
 			printf("[%d] %s\n", h->len, h->str);
 			h = h->next;
 	}
 	return (counter);
 }
-
-
