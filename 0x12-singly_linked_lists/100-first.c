8dcf4058d6def4e78cae8bceb8d71a222d728ae7
(nombre de usuario) <(correo de github)>
2020-03-24 06:25:43 +0000

    add 1st advance task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * tortuga - Prints beforer main function
+ *
+ * Return: Void
+ */
+
+void tortuga(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
