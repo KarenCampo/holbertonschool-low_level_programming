6e8ea3bcbe91a683e2204b081a98118573153758
(nombre de usuario) <(correo de github)>
2020-03-26 04:19:31 +0000

    add task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - Function that returns the sum of all dat(n) of a linked list
+ * @head: pointer to header of list
+ * Retun: sum
+ */
+int sum_listint(listint_t *head)
+{
+	unsigned int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
