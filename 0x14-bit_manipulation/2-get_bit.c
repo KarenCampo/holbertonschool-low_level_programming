4a6c819f18079939f038e01271a78e17edcbf66a
(nombre de usuario) <(correo de github)>
2020-03-30 05:13:28 +0000

    add task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to print in binary
+ * Return: Nothing but prints in stdout the binary series 
+ */
+void print_binary(unsigned long int n)
+{
+	int bit, c, size = 0;
+	unsigned long int dec = n;
+
+	if (n == 0)
+		_putchar('0');
+
+	while (dec)
+	{
+		size++;
+		dec >>= 1;
+	}
+
+	for (c = size - 1; c >= 0; c--)
+	{
+		bit = n >> c;
+			if (bit & 1)
+				_putchar('1');
+			else
+				_putchar('0');
+	}
+}
