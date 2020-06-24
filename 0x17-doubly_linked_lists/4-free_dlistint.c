fe3bfac0085a8ce9e6df4e1d7f3ff9d97aa57701
KarenCampo777 <1472@holbertonschool.com>
2020-06-24 17:31:08 +0000

    adding task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,20 @@
 #include "lists.h"
 
 /**
- * free_dlistint -
- *
- * Return: 
+ * free_dlistint - Frees  double linked list.
+ * @head: The head of the list
+ * Return: Nothing
  */
+
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *temp = head;
 
+	while (head != NULL)
+	{
+		head = head->next;
+		free(temp);
+		temp = head;
+	}
+	free(head);
 }
