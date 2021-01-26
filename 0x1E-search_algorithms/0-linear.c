0de750dd9a935d021babcb1747e375662a6b3a0c
KarenCampo777 <1472@holbertonschool.com>
2021-01-26 03:19:49 +0000

    adding tasks

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+  
+#include "search_algos.h"
+
+/**
+ * linear_search - It search for a given value in an array
+ * @size: The size of the array
+ * @value: The value that I need to find
+ * @array: header pointer
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t counter = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (counter < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", counter, array[counter]);
+		if (array[counter] == value)
+		{
+			return (counter);
+		}
+		counter++;
+	}
+	return (-1);
+	}
+
